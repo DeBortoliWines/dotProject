6b2d86c0745bc85f93075c691cfca6a643d56a60
Joel Giovinazzo <joelgiovinazzo@gmail.com>
2019-12-13 10:49:30 +1100

    database changes for companies and projects to support images

--- db/upgrade_latest.sql
+++ db/upgrade_latest.sql
@@ -9,3 +9,5 @@
 # 20191211
 ALTER TABLE `%dbprefix%tasks` MODIFY task_description LONGTEXT;
 ALTER TABLE `%dbprefix%task_log` MODIFY task_log_description LONGTEXT;
+ALTER TABLE `%dbprefix%projects` MODIFY project_description LONGTEXT;
+ALTER TABLE `%dbprefix%companies` MODIFY company_description LONGTEXT;
